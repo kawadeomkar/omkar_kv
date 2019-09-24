31970d52807554560adfa88cc4a9898657cdd921
kawadeomkar <kawade.omkar21@gmail.com>
2019-09-24 02:24:45 -0700

    plyvel start

--- server.py
+++ server.py
@@ -1,4 +1,12 @@
 import os
+import time
+
+print(os.environ['NAME'], os.getpid())
+
+if os.environ['NAME'] == 'master':
+	import plyvel
+	print(os.environ['DB'])
+	db = plyvel.DB(os.environ['DB'], create_if_missing=True)
 
 
 def master(env, start_response):
