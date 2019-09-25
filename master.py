c279ee5ef53ee5775da1a0c581552b10fd10b6aa
kawadeomkar <kawade.omkar21@gmail.com>
2019-09-25 14:58:23 -0700

    refactor

--- master.py
+++ master.py
@@ -10,10 +10,18 @@ if os.environ['NAME'] == 'master':
 
 
 def master(env, start_response):
-	key = db.get(env["REQUEST_URI"].encode('utf-8'))
+	for k, v in env.items():
+		print(k, v)
 	
-	if key is None:
+
+	if env["REQUEST_METHOD"] == "GET":
+		key = db.get(env["REQUEST_URI"].encode('utf-8'))
 	
+		# KEY ERROR
+		if key is None:
+			response = '404 Key not found'
+			start_response(response, [('Content-Type','text/plain')])
+			return [bytes(response, 'utf-8')]
 	
-	start_response('200 OK', [('Content-Type','text/html')])
+	start_response('200 OK', [('Content-Type','text/plain')])
 	return [b"OK"]
